9af37e9fa23946cf4816f2fd79697f41d41cee53
zxfzxyrxf <zhangxiaofei@atguigu.com>
2018-09-04 16:15:57 +0800

    update

--- src/filters/index.js
+++ src/filters/index.js
@@ -2,8 +2,10 @@
 自定义过滤器
  */
 import Vue from 'vue'
-import moment from 'moment'
+// import moment from 'moment'
+import format from 'date-fns/format'
 
 Vue.filter('date-format', function (value, format) {
-  return moment(value).format(format || 'YYYY-MM-DD HH:mm:ss')
+  // return moment(value).format(format || 'YYYY-MM-DD HH:mm:ss')
+  return format(value, format || 'YYYY-MM-DD HH:mm:ss')
 })
